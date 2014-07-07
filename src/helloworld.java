2b7bb02a0e0f3057aa50ac01dee081a0431bcd53
ChaoMing Chen <564506702@qq.com>
2014-07-07 21:18:09 +0800

    miglily第一次提交
    添加一行输出second line

--- src/helloworld.java
+++ src/helloworld.java
@@ -6,6 +6,7 @@ public class helloworld {
 	 */
 	public static void main(String[] args) {
        System.out.println("first line");
+       System.out.println("second line");//commited by minglily
 	}
 
 }
